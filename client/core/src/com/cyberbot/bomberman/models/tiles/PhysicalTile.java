902ea420339650f13634b793ef23360c31b6ecfb
Krzysztof 'RouNdeL' Zdulski <krzysztof.zdulski.stud@pw.edu.pl>
2020-05-14 20:25:14 +0200

    Optimize imports

--- client/core/src/com/cyberbot/bomberman/models/tiles/PhysicalTile.java
+++ client/core/src/com/cyberbot/bomberman/models/tiles/PhysicalTile.java
@@ -6,8 +6,6 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
-import static com.cyberbot.bomberman.utils.Constants.PPM;
-
 public abstract class PhysicalTile extends BaseTile {
     protected Body body;
     private World world;
