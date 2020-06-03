f804df9b92c0d1ed6c0c4f0037be4b1b64252919
Krzysztof 'RouNdeL' Zdulski <krzysztof.zdulski.stud@pw.edu.pl>
2020-06-03 15:52:49 +0200

    Add NetworkedGameplayController to GameScreen

--- client/core/src/com/cyberbot/bomberman/managers/GameScreenManager.java
+++ client/core/src/com/cyberbot/bomberman/managers/GameScreenManager.java
@@ -1,6 +1,10 @@
 package com.cyberbot.bomberman.managers;
 
+import com.badlogic.gdx.math.Vector2;
 import com.cyberbot.bomberman.Client;
+import com.cyberbot.bomberman.core.models.items.Inventory;
+import com.cyberbot.bomberman.core.models.net.Connection;
+import com.cyberbot.bomberman.core.models.net.data.PlayerData;
 import com.cyberbot.bomberman.core.models.tiles.MissingLayersException;
 import com.cyberbot.bomberman.screens.AbstractScreen;
 import com.cyberbot.bomberman.screens.GameScreen;
@@ -8,6 +12,7 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.HashMap;
 
 public final class GameScreenManager {
@@ -30,7 +35,11 @@ public final class GameScreenManager {
     private void initScreens() {
         screens = new HashMap<>();
         try {
-            screens.put(ScreenState.GAME, new GameScreen(app));
+            // TODO: Load game screen when a game start packet is received with proper data
+
+            PlayerData playerData = new PlayerData(-1, new Vector2(0, 0), new Inventory(), 0);
+            screens.put(ScreenState.GAME, new GameScreen(app, playerData, "./map/bomberman_main.tmx",
+                new Connection(12345, InetAddress.getLocalHost())));
         } catch (MissingLayersException | SAXException | ParserConfigurationException | IOException e) {
             e.printStackTrace();
         }
