e7ebe61ce8a3f642185ce43c44f24edca6dae425
Krzysztof 'RouNdeL' Zdulski <krzysztof.zdulski.stud@pw.edu.pl>
2020-06-05 21:00:12 +0200

    Remove LibGDX dependency from server

--- server/src/main/kotlin/com/cyberbot/bomberman/server/Server.kt
+++ server/src/main/kotlin/com/cyberbot/bomberman/server/Server.kt
@@ -1,10 +1,9 @@
 package com.cyberbot.bomberman.server
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-
 internal object Server {
     @JvmStatic
     fun main(args: Array<String>) {
-        LwjglApplication(Application())
+        val service = ServerService(8038)
+        Thread(service).start()
     }
 }
